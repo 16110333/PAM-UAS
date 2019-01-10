2d7e700f5d6bdc653643a2f7f83485332eef5f39
16110323 <moeh.darmawan@students.amikom.ac.id>
2019-01-10 10:20:24 +0700

    data2

--- berita/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-// mengkoneksikan ke database
-$server = "localhost";
-$server_username = "root";
-$server_password = "";
-$database_name =  "berita";
-
-$conn = new Mysqli($server, $server_username, $server_password, $database_name);
